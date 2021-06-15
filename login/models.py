f87993bcce4d8a71fdf6104293e29b84d05404a4
darz10 <you@example.com>
2021-06-15 14:11:05 +0500

    create models

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Модель пользователя """
+    
+    name_user = models.CharField(verbose_name='Имя пользователя', max_length=100)
+    photo_user = models.ImageField(verbose_name='Фотография пользователя', upload_to='media/photo_user/')
+    
